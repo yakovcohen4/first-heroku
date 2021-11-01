e5049d398f3c3c56672613a8cd63672716b23e87
yakovcohen4 <yakovc1431@gmail.com>
2021-11-01 15:55:01 +0200

    add front

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
-// Test import of a JavaScript module
-import { example } from '@/js/example'
-
-// Test import of an asset
-import webpackLogo from '@/images/webpack-logo.svg'
-
-// Test import of styles
-import '@/styles/index.scss'
-
-// Appending to the DOM
-const logo = document.createElement('img')
-logo.src = webpackLogo
-
-const heading = document.createElement('h1')
-heading.textContent = example()
-
-// Test a background image url in CSS
-const imageBackground = document.createElement('div')
-imageBackground.classList.add('image')
-
-// Test a public folder asset
-const imagePublic = document.createElement('img')
-imagePublic.src = '/assets/example.png'
+import './styles/index.scss'
+import {selectComponent} from "./js/selectComponent";
 
 const app = document.querySelector('#root')
-app.append(logo, heading, imageBackground, imagePublic)
+const options = [
+  {
+    value: '',
+    text: 'Select'
+  },
+  {
+    value: 'ip',
+    text: 'Get IP'
+  },
+  {
+    value: 'device',
+    text: 'Get Device'
+  },
+  {
+    value: 'os',
+    text: 'Get Operating System'
+  },
+  {
+    value: 'browser',
+    text: 'Get Browser'
+  }
+];
+
+const select = selectComponent(options);
+app.append(select);
